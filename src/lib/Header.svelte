01e12f069129d31a201aebc988a9de02aa0f2c42
andraObrien07 <comanan09@craighead.school.nz>
2024-04-23 09:46:14 +1200

    added picture of wall
    added pictures of wall, and alt tags for visual disabled people

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,9 @@
+<script>
+  export let color1 = "orange";
+  export let color2 = "darksalmon";
+</script>
+
 <header>
-  <h1 class="title">Welcome</h1>
+  <h1 style="background: {color1}" class="Walls1">Walls</h1>
+  <h2 style="background: {color2};" class="Walls2">Website Containing Walls</h2>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  import Header from "$lib/Header.svelte";
 </script>
 
 <Header />
@@ -7,9 +7,33 @@
 <main>
   <h2>SvelteKit</h2>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  <p>
+    Welcome to coding with SvelteKit, a modern JavaScript framework that makes
+    it easy to code great apps.
+  </p>
+  <div class="column">
+    <img src="art-deco.jpeg" alt="Art Deco Wall" />
+    <p>Art Deco Wall</p>
+    <p>$34.9</p>
+
+    <img src="flowers-wall.jpeg" alt="Wall With Flower" />
+    <p>Wall With Flowers</p>
+    <p>$24.9</p>
+
+    <img src="grey-run-down.jpeg" alt="Run Down Wall Painted Grey" />
+    <p>Run Down Wall Painted Grey</p>
+    <p>$4.9</p>
+
+    <img src="modified-white-brick.jpeg" alt="Brick Wall Painted White" />
+    <p>Brick Wall Painted White</p>
+    <p>$14.9</p>
+
+    <img src="orange-plaster.jpeg" alt="Plastered Wall Painted Orange" />
+    <p>Plastered Wall Painted Orange</p>
+    <p>$11.9</p>
+  </div>
 </main>
 
 <footer>
-  <p>&copy; Craighead Diocesan School 2024</p>
+  <p>&copy; Craighead Diocesan School 2024 @andrAComan</p>
 </footer>
